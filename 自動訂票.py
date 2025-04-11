51b1395b4ede4e10bd6d6a91a2bf52bc1ca6c7cc
Liam-TW <w19981031@gmail.com>
2025-04-11 14:42:46 +0800

    Update 自動訂票.py

--- 自動訂票.py
+++ 自動訂票.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_argument("--headless")
@@ -13,7 +15,8 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--user-data-dir=/tmp/chrome_user_data")
-driver = webdriver.Chrome(options=options)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://order.kingbus.com.tw/ORD/ORD_M_1510_OrderGo.aspx")
 
 # 填入基本資料
